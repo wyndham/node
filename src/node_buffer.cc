wyndham/node

commit 00b4b7bb97c84d19c644a4491fe98170ab9675a6

buffer: remove minor Buffer::Copy deoptimizations
* Omit ToObject() call. Buffer::Data() and Buffer::Length() know how
  to deal with Values.

* Don't check if the argument is undefined because it realistically
  never is and undefined->integer coercion achieves the same thing.

src/node_buffer.cc
@@ -398,13 +398,12 @@ Handle<Value> Buffer::Copy(const Arguments &args) {
     return ThrowTypeError("First arg should be a Buffer");
   }
 
-  Local<Object> target = args[0]->ToObject();
+  Local<Value> target = args[0];
   char* target_data = Buffer::Data(target);
   size_t target_length = Buffer::Length(target);
-  size_t target_start = args[1]->IsUndefined() ? 0 : args[1]->Uint32Value();
-  size_t source_start = args[2]->IsUndefined() ? 0 : args[2]->Uint32Value();
-  size_t source_end = args[3]->IsUndefined() ? source->length_
-                                              : args[3]->Uint32Value();
+  size_t target_start = args[1]->Uint32Value();
+  size_t source_start = args[2]->Uint32Value();
+  size_t source_end = args[3]->Uint32Value();
 
   if (source_end < source_start) {
     return ThrowRangeError("sourceEnd < sourceStart");

commit f0bad5a5cd2bdbddbd662b85df45e48f2bc40e91

buffer: Fix Buffer.copy() third arg default
Third argument should default to source.length, not 0.

Broken in 00b4b7bb97c84d19c644a4491fe98170ab9675a6.
  "buffer: remove minor Buffer::Copy deoptimizations"

src/node_buffer.cc
@@ -403,7 +403,8 @@ Handle<Value> Buffer::Copy(const Arguments &args) {
   size_t target_length = Buffer::Length(target);
   size_t target_start = args[1]->Uint32Value();
   size_t source_start = args[2]->Uint32Value();
-  size_t source_end = args[3]->Uint32Value();
+  size_t source_end = args[3]->IsUndefined() ? source->length_
+                                             : args[3]->Uint32Value();
 
   if (source_end < source_start) {
     return ThrowRangeError("sourceEnd < sourceStart");